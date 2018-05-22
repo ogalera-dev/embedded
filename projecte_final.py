277a7fec4434dc5a78ea86a2dead1f3302e5fe7a
Meriem Abjil <merimari051094@gmail.com>
2018-05-22 19:34:11 +0200

    Twitter

--- projecte_final.py
+++ projecte_final.py
@@ -11,6 +11,8 @@ import sys, locale, threading
 #import oauth, tweepy, sys, locale, threading 
 from time import localtime, strftime, sleep
 
+import tweepy
+
 
 ##################### Gmail
 
@@ -206,21 +208,21 @@ def facebook():
 
 def twitter(): 
     global follower
-    global api    #https://apps.twitter.com
-    consumer_key = "6Ov9ZYpGrYfCv3xomagiPEWB8"  # use your access key
-    consumer_secret = "SZkb5GyBnWw0O8DjaQ0dFhnHSoQXXEo1NQfIsWbNryNhVPlbtl"
-    access_key = "  998955377575251968-5VT2GrKdrNLavQlZwzdzIUc104wttUK"
-    access_secret = "7b6hHVpTA2ZVM61OL5ntSb5uImjOJ9lBSA9RDsxB155Xd"
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_key, access_secret)
-    api = tweepy.API(auth)
+	global api    #https://apps.twitter.com
+	consumer_key = "6Ov9ZYpGrYfCv3xomagiPEWB8"  # use your access key
+	consumer_secret = "SZkb5GyBnWw0O8DjaQ0dFhnHSoQXXEo1NQfIsWbNryNhVPlbtl"
+	access_key = "998955377575251968-5VT2GrKdrNLavQlZwzdzIUc104wttUK"
+	access_secret = "7b6hHVpTA2ZVM61OL5ntSb5uImjOJ9lBSA9RDsxB155Xd"
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
 
-    user = api.get_user(998955377575251968)  # your user id
-    print user.screen_name
-    print user.followers_count
-    follower = user.followers_count
-    print user.friends_count
-    print user.favourites_count
+	user = api.get_user('998955377575251968')  # your user id
+	print user.screen_name
+	print user.followers_count
+	follower = user.followers_count
+	print user.friends_count
+	print user.favourites_count
 
 
 # CONSULTEM EL GMAIL
